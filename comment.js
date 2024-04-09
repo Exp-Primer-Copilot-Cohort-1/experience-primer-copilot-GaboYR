fdcbac213b633a3ba55400ae6f2c1a7cf94ff10e
Gabriel YR <83891241+GaboYR@users.noreply.github.com>
2024-04-09 18:31:58 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,52 +1,71 @@
-//Create web server
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const path = require('path');
-
-//Use body-parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//Set up view engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-//Set up public folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-//Set up port
-const port = process.env.PORT || 3000;
-
-//Set up database
-const db = require('./config/database');
-db.connect(err => {
-    if (err) {
-        console.log('Unable to connect to database');
-        process.exit(1);
+// Create web server
+// Load the http module to create an http server.
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var querystring = require('querystring');
+var path = require('path');
+var root = __dirname;
+var comment = [];
+var server = http.createServer(function (req, res) {
+    var urlObj = url.parse(req.url, true);
+    var pathname = urlObj.pathname;
+    var query = urlObj.query;
+    var method = req.method;
+    if (pathname == '/') {
+        fs.readFile('./index.html', function (err, data) {
+            if (err) {
+                res.writeHead(404, {
+                    'Content-Type': 'text/html'
+                });
+                res.end('<h1>404 Not Found</h1>');
+            }
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            res.end(data);
+        });
+    } else if (pathname == '/comment') {
+        if (method.toUpperCase() == 'GET') {
+            var commentData = query.comment;
+            comment.push(commentData);
+            res.end(JSON.stringify(comment));
+        } else if (method.toUpperCase() == 'POST') {
+            var str = '';
+            req.on('data', function (data) {
+                str += data;
+            });
+            req.on('end', function () {
+                var postComment = querystring.parse(str).comment;
+                comment.push(postComment);
+                res.end(JSON.stringify(comment));
+            });
+        }
     } else {
-        console.log('Connected to database');
+        var filePath = path.join(root, pathname);
+        fs.exists(filePath, function (exists) {
+            if (!exists) {
+                res.writeHead(404, {
+                    'Content-Type': 'text/html'
+                });
+                res.end('<h1>404 Not Found</h1>');
+            } else {
+                fs.readFile(filePath, function (err, data) {
+                    if (err) {
+                        res.writeHead(500, {
+                            'Content-Type': 'text/html'
+                        });
+                        res.end('<h1>500 Server Error</h1>');
+                    }
+                    res.writeHead(200, {
+                        'Content-Type': 'text/html'
+                    });
+                    res.end(data);
+                });
+            }
+        });
     }
 });
-
-//Set up routes
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Comment' });
-});
-
-app.post('/submit', (req, res) => {
-    const { name, email, comment } = req.body;
-    const query = 'INSERT INTO comments (name, email, comment) VALUES (?, ?, ?)';
-    db.execute(query, [name, email, comment], (err, result) => {
-        if (err) {
-            res.send('Unable to submit comment');
-        } else {
-            res.send('Comment submitted');
-        }
-    });
-});
-
-//Listen on port
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+server.listen(3000, function () {
+    console.log('Server is running on port 3000');
 });
